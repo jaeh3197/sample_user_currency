f948f46bf59f35345d41cd1bd887b86d51bf8ef7
jaeh3197 <jaeh3197@naver.com>
2024-11-27 17:19:35 +0900

    feat: Lv 1 환전 요청 중간 테이블 생성
    UserCurrency 엔티티를 만들어 User 와 Currency 에 연관관계를 설정하였습니다.

---
+++ src/main/java/com/sparta/currency_user/entity/UserCurrency.java
@@ -0,0 +1,39 @@
+package com.sparta.currency_user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "user_currency")
+public class UserCurrency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "to_currency_id")
+    private Currency currency;
+
+    private Long amountInKrw;
+
+    private Long amountAfterExchange;
+
+    private String status;
+
+    public UserCurrency(User user, Currency currency, Long amountInKrw, Long amountAfterExchange, String status) {
+        this.user = user;
+        this.currency = currency;
+        this.amountInKrw = amountInKrw;
+        this.amountAfterExchange = amountAfterExchange;
+        this.status = status;
+    }
+
+    public UserCurrency() {
+    }
+}
